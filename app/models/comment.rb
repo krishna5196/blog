4b283a28a1b46528c5cf2c494fa707ec1425f2a9
Krishna Mehta <krishna.mehta>
2020-05-09 07:19:27 +0100

    Extra functionality

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,14 @@ class Comment < ApplicationRecord
   # has_many :comments, :foreign_key => comment_id, :dependent => :destroy
 
   validates_length_of :description, :maximum => 100
+
+  validate :validate_bad_words
+
+  include VerifyWord
+
+  def validate_bad_words
+    if check_for_bad_words(self.description)
+      errors.add(:base, "Body Contains Bad Words")
+    end
+  end
 end
